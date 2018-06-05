e59919f2ca70e480d899f379a96842162e2a23d6
ltt-gddxz <18810271756@163.com>
2018-06-05 20:39:46 +0800

    3

--- 3_backtrace.cpp
+++ 3_backtrace.cpp
@@ -6,26 +6,29 @@ using namespace std;
 class Solution {
 public:
 	/** 回溯产生子序列
-	  * patameters:
-	  *		strs: 保留所有可行的结果，以引用形式存在
-	  *		elem: 可供使用的基本元素
-	  *		flag: 访问标志，基本元素是否被访问过
-	  *		str: 当前的中间序列结果
-	  */
-	void get_per(vector<string> &strs, string elem, vector<bool> flag, string str) {
+	*   此函数的n和cur参数可以省去，但为了写成更加一般的模板形式，故在此加上
+	* patameters:
+	*		strs: 保留所有可行的结果，以引用形式存在，通过这个值来返回所有结果
+	*		elems: 可供使用的基本元素
+	*		visited: 访问标志，基本元素是否被访问过
+	*		n: 阶段总数
+	*		str: 当前的中间序列结果
+	*		cur: 当前阶段
+	*/
+	void get_perutation(vector<string> &strs, string elems, vector<bool> visited, int n, string str, int cur) {
 		// 回溯的边界条件
-		if (str.size() == flag.size()) {
+		if (cur == n) {
 			strs.push_back(str);
 			return;
 		}
-		
-		for (int i = 0; i<flag.size(); i++) {
-			if (!flag[i]) {
-				str.push_back(elem[i]);	// 尝试探索下一个可行步骤
-				flag[i] = true;		// 使用flag数组标志是否被访问过
-				get_per(strs, elem, flag, str);
+
+		for (int i = 0; i<visited.size(); i++) {
+			if (!visited[i]) {
+				str.push_back(elems[i]);	// 尝试探索下一个可行步骤
+				visited[i] = true;		// 使用flag数组标志是否被访问过
+				get_perutation(strs, elems, visited, n, str, cur+1);
 				str.pop_back();		// 返回上一层状态，继续探索其他可能
-				flag[i] = false;	// 清楚该情况的访问标志
+				visited[i] = false;	// 清楚该情况的访问标志
 			}
 		}
 	}
@@ -35,31 +38,29 @@ public:
 		if (str.size() == 0) return strs;
 
 		sort(str.begin(), str.end());
-		string elem = str;
-		vector<bool> flag(str.size(), false);
+
+		string elems = str;
+		vector<bool> visited(str.size(), false);
+		int n = str.size();
 		str = "";
-		get_per(strs, elem, flag, str);
+		int cur = 0;
+		get_perutation(strs, elems, visited, n, str, cur);
 
 		// 去重
 		map<string, int> mp;
-		vector<string> res;
-		for (int i = 0; i<strs.size(); i++) {
-			string temp = strs[i];
-			if (mp[temp] == 0) {
-				res.push_back(temp);
-				mp[temp] = 1;
-			}
-		}
+		sort(strs.begin(), strs.end());
+		vector<string>::iterator temp_end = unique(strs.begin(), strs.end());
+		strs.erase(temp_end, strs.end());
 
-		return res;
+		return strs;
 	}
 };
 
 
-int main() {
-	Solution *solver = new Solution();	
+int trackback_main() {
+	Solution *solver = new Solution();
 	vector<string> ans = solver->Permutation("abc");
-	
+
 	for (int i = 0; i < ans.size(); i++)
 		cout << ans[i] << " ";
 	cout << endl;
